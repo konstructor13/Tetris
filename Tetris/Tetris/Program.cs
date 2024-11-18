043d3fc7135adbb773e82a4fff68ac097c96413c
Евгений Паршаков <konstruction13@mail.ru>
2024-11-18 19:22:45 +0500

    Added Draw Function

---
+++ Tetris/Tetris/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tetris
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Draw(2, 3, '#');
+
+            Console.ReadKey();
+        }
+
+        static void Draw(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
